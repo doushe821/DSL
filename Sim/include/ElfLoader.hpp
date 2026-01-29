891f1f2e2c24c5f77e11c701bd39c0f5b04e332d
doushe821 <romachevskii.ae@phystech.edu>
2026-01-29 21:06:07 +0300

    [General] Cleaned up repo a bit

--- Sim/include/ElfLoader.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <cinttypes>
-#include <string>
-#include <vector>
-
-void loadElf(const std::string &ElfName, unsigned char* Memory, size_t MemoryLimit, size_t &EntryPoint);
